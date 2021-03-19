1efff9648e94e6d27bda8c04ee32b5dd6608cb8f
terribledevice <longlost@gmail.com>
2021-03-18 21:17:15 -0400

    added a console.log for the server link--makes it a little easier testing in vs code terminal

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const axios = require('axios');
 
 
@@ -17,4 +17,4 @@ sendGetRequest();
 })
 
 
-app.listen(PORT)
+app.listen(PORT, ()=> console.log(`Listening on http://localhost:${PORT}`))

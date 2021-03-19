c3abc4cd37456657a4c0bc23d37d1e365ffaa785
terribledevice <longlost@gmail.com>
2021-03-18 22:47:27 -0400

    added sass and sass middleware to project

---
+++ app/routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+module.exports = router;
+
+router.get('/', function (req, res) {
+    const sendGetRequest = async () => {
+        try {
+            const resp = await axios.get('https://jsonplaceholder.typicode.com/posts');
+            res.render('../views/index')
+            // res.send(resp.data[0].title);
+        } catch (err) {
+            res.status(404).send('Not found');
+        }
+    };
+    sendGetRequest();
+    })
--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
 const PORT = process.env.PORT || 5000;
-const axios = require('axios');
+const routes = require("./app/routes");
+const sassMiddleware = require('node-sass-middleware');
 
+const path = require('path');
+app.use(sassMiddleware({
+    src: __dirname + '/sass', 
+    dest: __dirname + '/public/stylesheets', 
+    debug: true, 
+    outputStyle: 'compressed' 
+  }),
+  express.static(path.join(__dirname, 'public')))
 
-app.get('/', function (req, res) {
-const sendGetRequest = async () => {
-    try {
-        const resp = await axios.get('https://jsonplaceholder.typicode.com/posts');
-        res.send(resp.data[0].title);
-    } catch (err) {
-        res.status(404).send('Not found')
-    }
-};
-sendGetRequest();
-})
+// Templating engine
+app.engine('hbs', exphbs({ extname: 'hbs' }));
+app.set('view engine', 'hbs');
 
-
-app.listen(PORT, ()=> console.log(`Listening on http://localhost:${PORT}`))
+app.use('/', routes);
+app.listen(PORT, ()=> console.log(`Listening on http://localhost:${PORT}`));

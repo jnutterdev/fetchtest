c212bd0f57e1b0c5e9413f828b58bcaa152a454f
terribledevice <longlost@gmail.com>
2021-03-20 13:39:02 -0400

    db stuff setup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mysql = require('mysql');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const routes = require("./app/routes");
@@ -27,6 +28,21 @@ app.engine('hbs', exphbs({
     defaultLayout: 'main', 
 }));
 app.set('view engine', 'hbs');
+
+// Connection pool
+const pool = mysql.createPool({
+    connectionLimit: 100,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+// Connect to DB
+pool.getConnection((err, conn) =>  {
+    if(err) throw err; // not connected
+    console.log(`Connected as ID ${conn.threadId}`);
+});
 
 app.use('/', routes);
 app.listen(PORT, ()=> console.log(`Listening on http://localhost:${PORT}`));
